a352ecfd14f6403faab06a435eb45bb1cbb6e8c5
unknown <you@example.com>
2023-12-12 00:58:53 +0900

    first commit

---
+++ src/main/java/com/example/villon_fe/VillonFeApplication.java
@@ -0,0 +1,13 @@
+package com.example.villon_fe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillonFeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VillonFeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/villon_fe/VillonFeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.villon_fe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillonFeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
